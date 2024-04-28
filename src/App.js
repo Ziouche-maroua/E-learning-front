0d090324adb3de1a01e29e046ce6e631f39b889c
Ziouche Maroua <142220454+Ziouche-maroua@users.noreply.github.com>
2024-04-28 07:23:24 +0100

    updating routes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,26 @@ import Chapter1 from './pages/Chapter1';
 import Chapter2 from './pages/Chapter2';
 import Chapter3 from './pages/Chapter3';
 import MotivationPage from './pages/MotivationPage';
+import SettingsPage from './pages/SettingsPage';
+import AccountPage from './pages/AccountPage';
+import ProfilePage from './pages/ProfilePage';
+import LogoutPage from './pages/LogoutPage';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/chapter1" element={<Chapter1 />} />
-        <Route exact path="/chapter2" element={<Chapter2 />} />
-        <Route exact path="/chapter3" element={<Chapter3 />} />
-        <Route path="/MotivationPage" element={<MotivationPage/>} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/chapter1" element={<Chapter1 />} />
+      <Route exact path="/chapter2" element={<Chapter2 />} />
+      <Route exact path="/chapter3" element={<Chapter3 />} />
+      <Route path="/MotivationPage" element={<MotivationPage />} />
+      <Route path="/SettingsPage" element={<SettingsPage />} />
+      <Route path="/AccountPage" element={<AccountPage />} />
+      <Route path="/ProfilePage" element={<ProfilePage />} />
+      <Route path="/LogoutPage" element={<LogoutPage />} />
+
       </Routes>
     </Router>
   );
