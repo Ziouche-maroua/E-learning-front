3f85ff94b957b72d83e4af4b578856bb335b2f7e
Ziouche Maroua <142220454+Ziouche-maroua@users.noreply.github.com>
2024-04-30 18:33:00 +0100

    updating topbar and global css

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -12,29 +12,19 @@ const TopBar = () => {
     setShowSidebar(!showSidebar);
   };
 
-  // Close the sidebar when clicking outside
-  const closeSidebar = () => {
-    if (showSidebar) {
-      setShowSidebar(false);
-    }
-  };
-
   return (
     <div className="w-full h-16 bg-blue-500 flex justify-between items-center px-4">
-      <div className="flex items-center space-x-2">
-        {/* Pass toggleSidebarOnHomeClick as prop */}
-        <button onClick={toggleSidebarOnHomeClick} className="text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-          </svg>
-        </button>
-        <Link to="/" className="text-white text-2xl font-bold flex items-center" onClick={closeSidebar}>
-          <span className="text-white">LearnVisual</span>
+      <div className="flex items-center">
+
+        <Link to="/" className="text-white text-2xl font-bold flex items-center">
+          <span className="text-white">FikrSight</span>
         </Link>
-        {/* Pass toggleSidebarOnHomeClick as prop */}
-        <LeftSideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} toggleOnHomeClick={toggleSidebarOnHomeClick} />
+       
+       
       </div>
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center flex-grow ml-8">
+        <Link to="/" className="text-white mr-4"> Home </Link>
+        <Link to="/courses" className="text-white mr-4"> Courses </Link>
         <div className="flex items-center relative">
           <input
             type="text"
@@ -44,9 +34,12 @@ const TopBar = () => {
           <div className="absolute left-3 top-2"> <img src={searchIcon} alt="Search" className="w-6 h-6" /> </div>
         </div>
       </div>
-      <div>
-       
-      </div>
+      <button onClick={toggleSidebarOnHomeClick} className="text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </button>
+      <LeftSideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar} toggleOnHomeClick={toggleSidebarOnHomeClick} />
     </div>
   );
 };
