841e26a3a554b97f655b31397382b83ea31632ee
Ziouche Maroua <142220454+Ziouche-maroua@users.noreply.github.com>
2024-05-02 15:53:16 +0100

    update top Bar component

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -22,9 +22,8 @@ const TopBar = () => {
        
        
       </div>
-      <div className="flex items-center flex-grow ml-8">
-        <Link to="/" className="text-white mr-4"> Home </Link>
-        <Link to="/courses" className="text-white mr-4"> Courses </Link>
+     
+       
         <div className="flex items-center relative">
           <input
             type="text"
@@ -33,7 +32,7 @@ const TopBar = () => {
           />
           <div className="absolute left-3 top-2"> <img src={searchIcon} alt="Search" className="w-6 h-6" /> </div>
         </div>
-      </div>
+     
       <button onClick={toggleSidebarOnHomeClick} className="text-white">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
