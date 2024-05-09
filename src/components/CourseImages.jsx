5935a4176e6ac13e20c192c0fd116ba45908e6f1
Ziouche Maroua <142220454+Ziouche-maroua@users.noreply.github.com>
2024-05-09 11:46:00 +0100

    installing mafs

--- src/components/CourseImages.jsx
+++ src/components/CourseImages.jsx
@@ -1,20 +1,43 @@
-// CourseImages.jsx
+// import React, { useEffect, useRef } from 'react';
+// import * as THREE from 'three';
 
-import React from 'react';
+// const CourseImage = () => {
+//     const containerRef = useRef();
 
-const ChapterImage = ({ path }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    className="w-16 h-16 text-blue-500"
-  >
-    <path
-      fillRule="evenodd"
-      d={path}
-      clipRule="evenodd"
-    />
-  </svg>
-);
+//     useEffect(() => {
+//         // Place the provided visualization script code here
 
-export default ChapterImage;
+//         // Example: Initialize Three.js scene and render it in the containerRef.current
+//         const scene = new THREE.Scene();
+//         const camera = new THREE.PerspectiveCamera(75, containerRef.current.offsetWidth / containerRef.current.offsetHeight, 0.1, 1000);
+//         const renderer = new THREE.WebGLRenderer();
+//         renderer.setSize(containerRef.current.offsetWidth, containerRef.current.offsetHeight);
+//         containerRef.current.appendChild(renderer.domElement);
+
+//         const geometry = new THREE.BoxGeometry();
+//         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+//         const cube = new THREE.Mesh(geometry, material);
+//         scene.add(cube);
+
+//         camera.position.z = 5;
+
+//         const animate = function () {
+//             requestAnimationFrame(animate);
+
+//             cube.rotation.x += 0.01;
+//             cube.rotation.y += 0.01;
+
+//             renderer.render(scene, camera);
+//         };
+
+//         animate();
+
+//         return () => {
+//             // Cleanup code if needed
+//         };
+//     }, []);
+
+//     return <div ref={containerRef} />;
+// };
+
+// export default CourseImage;
