4bcecfacfd8699a5578f75da708a5916ddfdebd0
066516 <ghmamdjeridinabil@gmail.com>
2024-05-19 22:50:43 +0100

    add user fetching

--- src/components/Account info.jsx
+++ src/components/Account info.jsx
@@ -6,13 +6,14 @@ import Cookies from "js-cookie";
 
 function Accountinfo() {
   // Déclarez des états locaux pour stocker les informations récupérées du backend
-  const [users, setStudents] = useState(null);
+  const [user, setStudents] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Fonction pour récupérer les informations de l'utilisateur depuis le backend
+    // Function to fetch user information from the backend
     const fetchStudentInfo = async () => {
       try {
-        // Envoi de la requête HTTP GET au backend pour récupérer les informations de l'utilisateur
+        // Send HTTP GET request to the backend to retrieve user information
         const studentData = await axios.get(
           "http://localhost:3001/api/student/info",
           {
@@ -22,21 +23,29 @@ function Accountinfo() {
               // Add other headers as needed
             },
           }
-        ); // Utilisez la fonction axiosGet avec le chemin relatif spécifique à votre backend
-        console.log(studentData.data);
-        setStudents(studentData); // Mise à jour de l'état avec les données récupérées
+        );
+
+        // Update the state with the retrieved data
+        await setStudents(studentData.data);
       } catch (error) {
         console.error(
           "Erreur lors de la récupération des informations utilisateur:",
           error
         );
+      } finally {
+        // Set loading to false after data is fetched or an error occurs
+        setLoading(false);
+        console.log(user);
       }
     };
 
-    // Appel de la fonction pour récupérer les informations de l'utilisateur lors du montage du composant
+    // Call the function to fetch user information when the component mounts
     fetchStudentInfo();
   }, []);
 
+  if (loading) {
+    return <div>Loading...</div>; // Render a loading indicator while fetching data
+  }
   return (
     <Grid
       templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
@@ -44,28 +53,28 @@ function Accountinfo() {
     >
       <FormControl id="firstName">
         <FormLabel>First Name</FormLabel>
-        <p>{users ? users.firstName : "Loading..."}</p>
+        <p>{user ? user?.student?.firstName : "Loading..."}</p>
       </FormControl>
       <FormControl id="lastName">
         <FormLabel>Last Name</FormLabel>
-        <p>{users ? users.lastName : "Loading..."}</p>
+        <p>{user ? user.lastName : "Loading..."}</p>
       </FormControl>
       <FormControl id="phoneNumber">
         <FormLabel>Phone Number</FormLabel>
-        <p>{users ? users.phoneNumber : "Loading..."}</p>
+        <p>{user ? user.phoneNumber : "Loading..."}</p>
       </FormControl>
       <FormControl id="emailAddress">
         <FormLabel>Email Address</FormLabel>
-        <p>{users ? users.Emailadress : "Loading..."}</p>
+        <p>{user ? user?.student?.user?.email : "Loading..."}</p>
       </FormControl>
 
       <FormControl id="city">
         <FormLabel>City</FormLabel>
-        <p>{users ? users.City : "Loading..."}</p>
+        <p>{user ? user.City : "Loading..."}</p>
       </FormControl>
       <FormControl id="university">
         <FormLabel>University</FormLabel>
-        <p>{users ? users.University : "Loading..."}</p>
+        <p>{user ? user.University : "Loading..."}</p>
       </FormControl>
     </Grid>
   );
