6f5edf31be038b6702842f649c833700be49515a
066516 <ghmamdjeridinabil@gmail.com>
2024-05-22 21:38:10 +0100

    fixe some style

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logImage from "../assets/images/log-image.jpg";
 import emailIcon from "../assets/images/email.png";
 import { useForm } from "react-hook-form";
@@ -17,6 +17,7 @@ const SignUp = () => {
     register,
     formState: { errors },
   } = useForm();
+  const navigate = useNavigate();
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -44,6 +45,11 @@ const SignUp = () => {
         console.log(response.data);
         toast.success("Successful registration");
         await Cookies.set("token", response.data.token); // Token will expire in 7 days
+        const route = Cookies.get("redirectAfterSignup");
+        if (route) {
+          navigate(route);
+          Cookies.remove("redirectAfterSignup");
+        } else navigate("/");
       }
     } catch (error) {
       if (error.response && error.response.status === 400) {
@@ -114,8 +120,11 @@ const SignUp = () => {
                   className="pl-10 pr-4 py-2 w-full bg-[#ffffff] rounded-md focus:outline-none focus:ring-2 focus:ring-[#67adee]"
                   {...register("last_name", {
                     required: "Please enter your last name",
-                    
-                    minLength: { value: 3, message: "Last name must be at least 3 characters" }
+
+                    minLength: {
+                      value: 3,
+                      message: "Last name must be at least 3 characters",
+                    },
                   })}
                 />
                 {errors.last_name && <span>{errors.last_name.message}</span>}
