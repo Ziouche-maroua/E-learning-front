fdbb0c3dfe1f128a39863b51424cbe72946557ab
Ziouche Maroua <142220454+Ziouche-maroua@users.noreply.github.com>
2024-05-28 18:02:03 +0100

    fixing..

--- src/pages/Chapter2.jsx
+++ src/pages/Chapter2.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import TopBar from '../components/TopBar'; 
 
 import Chatbox from '../components/ChatBox'; 
-import DefApp from './DefApp';
-
-
-
 import DefApp from './DefApp'; 
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
