2ab2ff63dc5107c6986eb16aa86afe7290f40433
Maroua Ziouche <maroua.ziouche@etu.usthb.dz>
2024-07-03 15:46:51 +0100

    updates

--- src/pages/FuturModules.jsx
+++ src/pages/FuturModules.jsx
@@ -11,11 +11,13 @@ const FuturModules = () => {
       <div className="pt-16 min-h-screen"> {/* Set minimum height to screen height */}
         <div className="container pt-16 mx-auto px-4 py-12">
           <div className="flex flex-col sm:flex-row items-center justify-start"> {/* Adjust flex direction for small screens */}
-          <FloatingImage
-              src={futureModuleImage}
-              alt="futur modules"
-              className='w-[563.701px] h-[350px] rounded-md'
-            />
+            <div className="w-[200px] h-[200px] sm:w-[350px] sm:h-[350px] rounded-full overflow-hidden flex-shrink-0 mr-4">
+              <FloatingImage
+                src={futureModuleImage}
+                alt="futur modules"
+                className='object-cover w-full h-full'
+              />
+            </div>
             <div>
               <p className="text-2xl sm:text-4xl font-bold varblue-text">We will provide this module as soon as possible.</p>
             </div>
